582addc83ffc09758353a15af073f512194d67c2
Mattazz <matthewazada101@gmail.com>
2025-03-21 08:59:07 -0400

    Add conditional rendering for product image in ProductCardMatt component if noimage

--- src/components/ProductCardMatt.tsx
+++ src/components/ProductCardMatt.tsx
@@ -7,11 +7,14 @@ interface Product {
     source: string;
 }
 
-function ProductCardMatt({product}: {product: Product}){
-    return(
-        <div className="rounded-md w-80 bg-white flex flex-col gap-4 items-center box-shadow-black p-4 duration-700 hover:scale-105">
+function ProductCardMatt({ product }: { product: Product }) {
+
+
+    return (
+        <div className={`rounded-md w-80 bg-white flex flex-col gap-4 items-center box-shadow-black p-4 duration-700 hover:scale-105 ${product.image && product.image.includes("noimage") ? "hidden" : ""}`}>
             <img className="w-40" src={product.image} alt="" />
             <h3 className="font-bold">{product.title}</h3>
+            <p>{product.price}</p>
             <p>{product.price}</p>
             {/* <p>{<p>{product.salePrice != null && `${product.salePrice}`}</p>}</p> */}
             <p className="bg-black text-white p-2 rounded-md">{product.source}</p>
