c86a9d39308638bdb66193395b32a791381e0a70
Angkit Khadka <khadkaankit85@gmail.com>
2025-03-27 18:09:40 -0400

    new config to add backend url accordin to the env

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,4 @@
-const backendUrl = "http://localhost:3000/";
+const environment = import.meta.env.VITE_ENVIRONMENT
+const backendUrl = environment === "production" ? import.meta.env.VITE_BACKEND_URL : "http://localhost:3000/";
 
 export { backendUrl };
